74f783f7250e2634f270db6d47a8f1785943ab5e
Hotlytia M. B. Siregar <100424330+Hotlytia27@users.noreply.github.com>
2023-12-07 15:59:21 +0700

    Add files via upload

--- js/index.js
+++ js/index.js
@@ -1,28 +1,48 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+const express = require("express")
 
-// Next/previous controls
-function plusSlides(n) {
-  showSlides(slideIndex += n);
-}
+// init express
+const app = express()
+const port = 3000
+const bodyParser = require("body-parser")
+const db = require("./connection")
+const response = require("./response")
+const router = ("./routers/router")
+app.use(bodyParser.json())
 
-// Thumbnail image controls
-function currentSlide(n) {
-  showSlides(slideIndex = n);
-}
+// Home Route
+app.get("/home",(req, res) => {
+    response(200, "ini data", "ini message", res)
+})
 
-function showSlides(n) {
-  let i;
-  let slides = document.getElementsByClassName("mySlides");
-  let dots = document.getElementsByClassName("dot");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
-  for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
-  }
-  for (i = 0; i < dots.length; i++) {
-    dots[i].className = dots[i].className.replace(" active", "");
-  }
-  slides[slideIndex-1].style.display = "block";
-  dots[slideIndex-1].className += " active";
-}
+// Isu Route
+app.get("/isu",(req, res) => {
+    response(200, "oke bisa","Berita terupdate capres cawapres", res)
+})
+
+// Pilpres Route
+app.get("/pilpress",(req, res) => {
+    const id_paslon = req.params.id_paslon
+    response(200, "Ganjar", `ambil by id ${id_paslon}`, res)
+})
+
+// About Route
+
+app.get("/about", (req, res) => {
+    response(200, "ketua tim", "BE", res)
+})
+app.post("/berita",(req, res) => {
+    response(200, "Ini berita Pilpres", res)
+})
+
+app.put("/berita",(req, res) => {
+    response(200, "Update Data", res)
+})
+
+app.delete("/berita",(req, res) => {
+    response(200, "Delete data", res)
+}) 
+
+app.listen(port, () => {
+    console.log("Server run well") 
+})
+
