913e460eab4e9a2ceabb06371d3d5f981673eea1
TheodorDani <63799480+TheodorDani@users.noreply.github.com>
2023-12-11 12:39:58 +0700

    sdh pakai squil

--- db.js
+++ db.js
@@ -1,71 +1,23 @@
-// Import required modules
-const express = require('express');
-const mysql = require('mysql');
+const Sequelize = require('sequelize');
 
-// Create an instance of Express.js
-const app = express();
-
-// Create a MySQL connection pool
-const pool = mysql.createPool({
+// Create a Sequelize instance with connection details
+const sequelize = new Sequelize('berita_capres', 'root', '', {
   host: 'localhost',
-  user: 'root',
-  password: '',
-  database: 'berita_capres'
+  dialect: 'mysql'
 });
 
-// Handle database connection errors
-pool.on('error', (err) => {
-  console.error('MySQL connection error:', err);
+
+// Define a model for the "berita" table
+const news = sequelize.define('berita', {
+  id_berita: { type: Sequelize.DataTypes.INTEGER, primaryKey: true },
+  title_isu: { type: Sequelize.DataTypes.TEXT, allowNull: false },
+  upload_date: { type: Sequelize.DataTypes.TEXT, allowNull: false },
+  gambar_isu: { type: Sequelize.DataTypes.STRING(255), allowNull: false },
+  isu: { type: Sequelize.DataTypes.TEXT, allowNull: false },
+  id_paslon: { type: Sequelize.DataTypes.INTEGER, allowNull: false },
 });
 
-// Define a route to test the database connection
-app.get('/test', (req, res) => {
-  // Get a connection from the pool
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting MySQL connection:', err);
-      return res.status(500).json({ error: 'Failed to get MySQL connection' });
-    }
-
-    // Execute a sample query to test the connection
-    connection.query('SELECT CURRENT_USER() AS result', (err, results) => {
-      // Release the connection back to the pool
-      connection.release();
-
-      if (err) {
-        console.error('Error executing MySQL query:', err);
-        return res.status(500).json({ error: 'Failed to execute MySQL query' });
-      }
-
-      // Send the query result as a response
-      res.json({ result: results[0].result });
-    });
-  });
-});
-
-app.get('/news', (req, res) => {
-  const query = 'SELECT title_isu, upload_date, gambar_isu, isu FROM berita';
-
-  pool.getConnection((err, connection) => {
-    if (err) {
-      console.error('Error getting MySQL connection:', err);
-      return res.status(500).json({ error: 'Failed to get MySQL connection' });
-    }
-
-    connection.query(query, (err, results) => {
-      connection.release();
-
-      if (err) {
-        console.error('Error executing MySQL query:', err);
-        return res.status(500).json({ error: 'Failed to execute MySQL query' });
-      }
-
-      res.json(results);
-    });
-  });
-});
-
-// Start the server
-app.listen(3000, () => {
-  console.log('Server started on port 3000');
-});
+module.exports = {
+  sequelize,
+  news,
+};
--- index.js
+++ index.js
@@ -1,23 +1,31 @@
-const dbConfig = require('../config/db.config');
-const Sequelize = require('sequelize');
-const sequelize = new Sequelize(
-    dbConfig.DB, 
-    dbConfig.USER, 
-    dbConfig.PASSWORD, {
-        host: dbConfig.HOST,
-        dialect: dbConfig.dialect,
-        operatorAlias: false,
-        pool: {
-            max: dbConfig.pool.max,
-            min: dbConfig.pool.min,
-            acquire: dbConfig.pool.acquire,
-            idle: dbConfig.pool.idle
-        },
-    });
-const db = {};
-db.Sequelize = Sequelize;
-db.sequelize = sequelize;
+const express = require('express');
+const router = express.Router();
+const { sequelize, news } = require('./db'); // Import the 'news' model from db.js
 
-// define semua models yang ada pada aplikasi
-db.capstone_project_be_grup27 = require('./book.model')(sequelize, Sequelize);
-module.exports = db;
+const app = express();
+
+async function getNews() {
+  try {
+    // Use the 'news' model directly to perform queries
+    const results = await news.findAll({
+      attributes: ['title_isu', 'upload_date', 'gambar_isu', 'isu'],
+    });
+
+    return results;
+  } catch (error) {
+    console.error('Error retrieving news:', error);
+    return [];
+  }
+}
+
+app.get('/news', async (req, res) => {
+  const newsData = await getNews();
+  res.json(newsData);
+});
+
+// Start the server
+app.use('/', router);
+const port = 8080;
+app.listen(port, () => {
+  console.log(`Server started on port ${port}`);
+});
